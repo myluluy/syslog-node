4bd7d3f9919658c8959d1513e6b446a3bbc5bec6
myluluy <myluluy@gmail.com>
2018-12-27 13:06:14 +0800

    更新test

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,2 @@
-let syslog = require('../build/Release/syslog')
-let n = Date.now();
-console.log(syslog);
-syslog.syslog('test tag', syslog.option().LOG_PID, syslog.facility().LOG_USER, syslog.priority().LOG_ERR, 'test message');
-// for(let i = 0; i< 1000; i++) {
-//   syslog('test tag', 'LOG_PID', 'LOG_LOCAL5', 'LOG_ERR', 'test message', (res)=> {
-//   })
-// }
-// console.log(Date.now() - n)
-// let m = Date.now();
-// for(let i = 0; i< 1000; i++) {
-//     console.log('====')
-// }
-// console.log(Date.now() - m)
+let Syslog = require('../')
+Syslog.syslog('xx-xx', Syslog.option.LOG_PID, Syslog.facility.LOG_USER, Syslog.priority.LOG_NOTICE, 'log message')
